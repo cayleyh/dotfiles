0af0ce91279be7f192d5a6d7088218722fcf328d
Cayley Humphries <cayley.humphries@gmail.com>
2015-10-28 13:44:43 -0700

    fix install paths

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 # TODO: Install required packages (brew git completion, brew bash_completion, etc)
 
 # install global gitignore
-echo '. git/.gitignore_global' > ~/.gitignore_global
+cp ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
 # install general profile
-echo '. ./bash/.profile' > ~/.profile
+echo '. ~/.dotfiles/bash/.profile' > ~/.profile
 source ~/.profile
