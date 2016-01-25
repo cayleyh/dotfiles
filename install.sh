893805349fe947c0b94aab9c611f46ae0d9d134a
Cayley Humphries <cayley.humphries@gmail.com>
2016-01-25 13:34:14 -0800

    Add gem config to fix El Cap issues

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 # TODO: https://github.com/milkbikis/powerline-shell
-# TODO: Install required packages (brew git completion, brew bash_completion, etc)
+# TODO: Install required packages (brew git completion, brew bash_completion, dotenv gem, etc)
 
 # install global gitignore
 cp ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
+
+# install local environment config files
+cp ~/.dotfiles/bash/configs/.gemrc ~/.gemrc
 
 # install general profile
 echo '. ~/.dotfiles/bash/.profile' > ~/.profile
