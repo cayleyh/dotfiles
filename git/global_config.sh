674c2b41c85bc71ffcd95281655ee29a42544527
Cayley Humphries <cayley.humphries@gmail.com>
2017-03-20 12:26:32 -0700

    Minor adjustment to not ignore .sql files in git by default

--- git/global_config.sh
+++ git/global_config.sh
@@ -1,1 +1,2 @@
+# Consider using core.eol instead: https://stackoverflow.com/questions/3206843/how-line-ending-conversions-work-with-git-core-autocrlf-between-different-operati/3209806#3209806
 git config --global core.autocrlf input
